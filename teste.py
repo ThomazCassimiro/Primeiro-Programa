7abc8c09f38da7473d149d2944bf686407e1dd56
Thomaz <thomaz.fonseca@aluno.senai.br>
2025-05-23 20:04:55 -0300

    Novo teste

--- teste.py
+++ teste.py
@@ -5,3 +5,8 @@ print(f'Olá {nome}, seja bem-vindo(a)')
 idade = int(input("Digite sua idade: "))
 
 print(f"Você tem {idade} anos")
+
+if idade < 18:
+    print(f"{nome}, você é menor de idade.")
+else:
+    print(f"{nome}, você é a maior de idade.")
