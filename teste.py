642c624e1bcb67a42b519b0a163341750fdad18e
ThomazCassimiro <130711793+ThomazCassimiro@users.noreply.github.com>
2025-05-23 21:35:57 -0300

    Versão revisada para 10 segundos

--- teste.py
+++ teste.py
@@ -7,6 +7,6 @@ idade = int(input("Digite sua idade: "))
 print(f"Você tem {idade} anos")
 
 if idade < 18:
-    print(f"{nome}, você é menor de idade.")
+    print(f"{nome}, você é menor de idade, pois vc tem {idade} anos")
 else:
     print(f"{nome}, você é a maior de idade.")
